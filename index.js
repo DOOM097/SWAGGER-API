498eaa911f9c2bfb9d505fb19b45591c746fa369
DOOM097 <98549250+DOOM097@users.noreply.github.com>
2023-10-03 15:06:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,51 +1,38 @@
 const express = require('express');
 const app = express();
-const bookRoutes = require('./routes/bookRoutes'); // Импортируйте маршруты
-const swaggerJsdoc = require("swagger-jsdoc")
-const swaggerUi = require("swagger-ui-express")
+const bookRoutes = require('./routes/bookRoutes');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 // Настройки Express
 app.use(express.json());
 
 // Подключение маршрутов
 app.use('/api', bookRoutes); // Указываем префикс для маршрута
 
-
-
-const options={
-  definition:{
-    openapi: "3.0.0",
-    info:{
-      title: "Library Example Express API with Swagger",
-      version:"0.1.0",
-      description:
-        "This is a simple CRUD API application made wuth Express and documented with Swagger",
-
+// Генерация спецификации Swagger
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Library Example Express API with Swagger',
+      version: '0.1.0',
+      description: 'This is a simple CRUD API application made with Express and documented with Swagger',
     },
-    servers:[
+    servers: [
       {
-        url: "http://localhost:3000/",
+        url: 'http://localhost:3000/',
         description: 'Development server',
       },
     ],
   },
-  apis:["./routes/*"],
+  apis: ['./routes/*'], // Путь к вашим файлам маршрутов
 };
 
 const specs = swaggerJsdoc(options);
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(specs)
-);
 
-
-
-
-
-
-
-
-
+// Роут для документации Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // Запуск сервера
 const PORT = process.env.PORT || 3000;
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
- 
 const bookController = require('../controllers/bookController');
 
-
+/**
+ * @swagger
+ * /api/books:
+ *   get:
+ *     summary: Получить список всех книг
+ *     responses:
+ *       200:
+ *         description: Успешный ответ
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ */
 router.get('/books', bookController.getAllBooks);
 
 module.exports = router;
